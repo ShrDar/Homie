9780b0b401f23bd616577ac7c522d9bd26bd87b8
ShrDar <np03cs4a220080@heraldcollege.edu.np>
2025-01-31 00:09:04 +0545

    Added: Backend Codes

--- homie-server/server.js
+++ homie-server/server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import cors from "cors";
+
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+// app.use('/record', records);
 
 app.get("/api/home", (req, res) => {
     res.json({ message: "Hello World" });
