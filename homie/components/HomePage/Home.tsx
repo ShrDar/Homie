61d4733dc0c63511db83682d92fd0e2c9574181f
ShrDar <np03cs4a220080@heraldcollege.edu.np>
2025-02-03 14:01:28 +0545

    Added: Firebase Configured in the app for real-time data manipulation

--- homie/components/HomePage/Home.tsx
+++ homie/components/HomePage/Home.tsx
@@ -2,12 +2,28 @@ import Image from "next/image";
 import EntryBtn from "../Button/EntryBtn";
 import { logout } from "@/actions/auth";
 import { auth } from "@/auth";
-
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '@/config/firebase.js'
 
 export default async function HomePage() {
     
     const session = await auth();
+
+    // const commentCollection = collection(db, "Comment");
+    // try {
+    //     const data = await getDocs(commentCollection);
+    //     const comments = data.docs.map((doc) => (
+    //         {
+    //             ...doc.data(), 
+    //             id: doc.id
+    //         }
+    //     ));
+    //     console.log(comments);
+    // } catch(err) {
+    //     console.log(err);
+    // }
     
+
     return (
         <div className="homeContainer bg-bgPrimary text-fontPrimary z-[5] sulphur h-screen w-screen flex flex-col items-center justify-center">
             <p>Home Page</p>
