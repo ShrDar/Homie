cb841677fd097464b23d6ac921544f335fc9e571
ShrDar <np03cs4a220080@heraldcollege.edu.np>
2025-02-11 00:28:51 +0545

    Fixed: Errors debugged

--- homie/auth.ts
+++ homie/auth.ts
@@ -7,8 +7,6 @@ import bcrypt from "bcryptjs";
 import { prisma } from "./prisma";
 import { saltAndHashPassword } from "./utils/helper";
 
-export const runtime = "nodejs"
-
 export const {
   handlers: { GET, POST },
   signIn,
