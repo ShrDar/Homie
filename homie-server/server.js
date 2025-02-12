31f4f90c69b08ce66a51bcac058d894d3f939c5b
ShrDar <np03cs4a220080@heraldcollege.edu.np>
2025-02-12 09:48:10 +0545

    Added: Display text Backend

--- homie-server/server.js
+++ homie-server/server.js
@@ -6,8 +6,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 8080;
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Homie Server");
+})
 
 app.use(cors());
 app.use(express.json());
